ff238d5ada72137eacc6aa8cf487cbeab2494c34
Yonatan Karp-Rudin <yonvata@gmail.com>
2023-11-02 07:50:09 +0100

    Add bug example project to repository
    This commit adds an example of the bug described in https://github.com/diffplug/spotless/issues/1865

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+    id("com.diffplug.spotless") version "6.22.0"
+}
+
+repositories {
+    mavenCentral()
+}
+allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.diffplug.spotless")
+
+    spotless {
+        kotlin {
+            target(
+                fileTree("${project.rootDir}/$name") {
+                    include("**/*.kt")
+                },
+            )
+            // see https://github.com/shyiko/ktlint#standard-rules
+            trimTrailingWhitespace()
+            ktlint("1.0.1").editorConfigOverride(
+                mapOf(
+                    "ktlint_standard_package-name" to "disabled",
+                    "ktlint_standard_enum-entry-name-case" to "disabled",
+                    "ktlint_standard_trailing-comma" to "disabled",
+                    "ktlint_standard_annotation" to "disabled",
+                    "ktlint_standard_no-unused-imports" to "disabled",
+                    "ktlint_standard_trailing-comma-on-call-site" to "disabled",
+                    "ktlint_standard_trailing-comma-on-declaration-site" to "disabled",
+                ),
+            )
+            diktat("1.2.3").configFile("${rootProject.rootDir}/config/diktat/diktat-analysis.yml")
+        }
+    }
+}
---
+++ module1/src/main/kotlin/DragonSlayingStrategy.kt
@@ -0,0 +1,5 @@
+package com.yonatankarp
+
+internal fun interface DragonSlayingStrategy {
+    fun execute()
+}
